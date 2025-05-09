e0ca938fbc3703c4a41fcdd42711099b955d9fce
00000921 <00000921@uca.edu.sv>
2025-05-08 19:33:15 -0600

    modify the repositories files

--- labo02/src/main/java/com/example/labo02/Domain/Repository/ClienteRepository.java
+++ labo02/src/main/java/com/example/labo02/Domain/Repository/ClienteRepository.java
@@ -1,4 +1,11 @@
 package com.example.labo02.Domain.Repository;
 
-public class ClienteRepository {
+import com.example.labo02.Domain.Entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 }
--- labo02/src/main/java/com/example/labo02/Domain/Repository/EmpleadoMentorRepository.java
+++ labo02/src/main/java/com/example/labo02/Domain/Repository/EmpleadoMentorRepository.java
@@ -1,4 +1,11 @@
 package com.example.labo02.Domain.Repository;
 
-public class EmpleadoMentorRepository {
+import com.example.labo02.Domain.Entities.EmpleadoMentor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmpleadoMentorRepository extends JpaRepository<EmpleadoMentor, UUID> {
 }
