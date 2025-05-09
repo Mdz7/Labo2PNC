38e8d8294bac373f76141a5a2c6ab178a3d0d647
00000921 <0000921@uca.edu.sv>
2025-05-08 20:03:35 -0600

    modify the repositories files 2

--- labo02/src/main/java/com/example/labo02/Domain/Repository/IndustriaRepository.java
+++ labo02/src/main/java/com/example/labo02/Domain/Repository/IndustriaRepository.java
@@ -1,4 +1,11 @@
 package com.example.labo02.Domain.Repository;
 
-public class IndustriaRepository {
+import com.example.labo02.Domain.Entities.Industria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IndustriaRepository extends JpaRepository<Industria, UUID> {
 }
--- labo02/src/main/java/com/example/labo02/Domain/Repository/ProyectoTecnologiaRepository.java
+++ labo02/src/main/java/com/example/labo02/Domain/Repository/ProyectoTecnologiaRepository.java
@@ -1,4 +1,11 @@
 package com.example.labo02.Domain.Repository;
 
-public class ProyectoTecnologiaRepository {
+import com.example.labo02.Domain.Entities.ProyectoTecnologia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProyectoTecnologiaRepository extends JpaRepository<ProyectoTecnologia, UUID> {
 }
